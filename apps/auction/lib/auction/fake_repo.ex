0e0f9291d669e5f9f6f787c0da5b915b453ab648
zentauro <pedrogomezmartin1999@gmail.com>
2020-03-31 03:35:40 +0200

    9 done

---
+++ apps/auction/lib/auction/fake_repo.ex
@@ -0,0 +1,44 @@
+defmodule Auction.FakeRepo do
+  alias Auction.Item
+
+  @items [
+    %Item{
+      id: 1,
+      title: "My first item",
+      description: "A tasty item sure to please",
+      ends_at: ~N[2020-01-01 00:00:00]
+    },
+    %Item{
+      id: 2,
+      title: "WarGames Bluray",
+      description: "The best computer movie of all time, now on Bluray!",
+      ends_at: ~N[2018-10-15 13:39:35]
+    },
+    %Item{
+      id: 3,
+      title: "U2 - Achtung Baby on CD",
+      description: "The sound of 4 men chopping down The Joshua Tree",
+      ends_at: ~N[2018-11-05 03:12:29]
+    },
+    %Item{
+      id: 4,
+      title: "Mario bros",
+      description: "NES Mario bros",
+      ends_at: ~N[2023-12-01 11:03:44]
+    }
+  ]
+
+  def all(Item), do: @items
+
+  def get!(Item, id) do
+    Enum.find(@items, fn item -> item.id === id end)
+  end
+
+  def get_by(Item, map) do
+    Enum.find(@items, fn item ->
+      Enum.all?(Map.keys(map), fn key ->
+        Map.get(item, key) === map[key]
+      end)
+    end)
+  end
+end
